32826d318a5a2a52c08fabf800516b5b5dcd9315
Noiz1212 <mortenhilti@gmail.com>
2018-01-09 13:00:06 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        System.out.println(list);
+    }
+}
